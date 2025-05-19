8a6843d6a6316c44b33a0e78bd5fee9677647ad6
Swati lahane <101633697+SwatiLahane@users.noreply.github.com>
2025-05-19 13:59:30 +0530

    Update TestRunner1.java

--- BakingProject/src/test/java/StepDefinition/TestRunner1.java
+++ BakingProject/src/test/java/StepDefinition/TestRunner1.java
@@ -5,21 +5,16 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-@RunWith(Cucumber.class)
-@CucumberOptions
+@RunWith(Cucumber.class)				
+	@CucumberOptions(features={"src/test/java/FeatureFiles1/"},
+	glue={"StepDefinition"},
+	//tags = "@High",
+	plugin = { "pretty", "html:target/site/cucumber-pretty.html",
+			"json:target/site/cucumber-report.json",
+			 "junit:target/site/cucumber-results.xml",
+	"json:target/cucumber.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
 
-(
-		features="src/test/java/FeatureFiles1/",
-		glue={"StepDefinition"},
-		
-		dryRun=false,
-		monochrome=false,
-		plugin= {"pretty","html:target/HtmlReport/index.html",
-				
-				 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }) 
-		
+	public class TestRunner1 {
 
-		
-		public class TestRunner1 {
-	
 	}
+	
